7a1084aecfa22c9734db62a6269e723471d32e46
Isaac Yanez <isaac231467@gmail.com>
2024-01-10 20:33:20 -0600

    small application asking user their fav number

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 using namespace std;
+int main () 
+{
 
-int main(){
-    cout << "Hello  World!" << endl;
+    int favorite_number;
+
+    cout << "Enter your favorite number between 1 and 100: ";
+
+    cin >> favorite_number;
+
+    cout << "Amazing!! That's my favorite number too!" << endl;
+
     return 0;
 }
